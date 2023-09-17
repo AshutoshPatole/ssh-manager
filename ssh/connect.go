0f88d1b7037520d16b9ca2e736d31c3d45d2aeda
AshutoshPatole <apatole479@gmail.com>
2023-09-17 13:10:50 +0530

    add prompt colors

--- ssh/connect.go
+++ ssh/connect.go
@@ -21,6 +21,18 @@ func Connect(server, user string) {
 		return
 	}
 
+	// Set your desired prompt colors using properly escaped ANSI escape codes
+	prompt := "\\[\\033[1;31m\\]\\[\\033[42m\\]\\u@\\h \\[\\033[0;36m\\]\\w\\[\\033[0m\\]\\$"
+	fmt.Println(prompt)
+
+	// Construct a single SSH command to set the new PS1 configuration in ~/.bashrc
+	exec.Command(
+		"ssh",
+		user+"@"+server,
+		fmt.Sprintf(`sed -i '/^export PS1=/d' ~/.bashrc && echo 'export PS1="%s"' >> ~/.bashrc`, prompt),
+	).Run()
+
+	// Modify the SSH command to set the prompt colors
 	sshCommand := exec.Command("ssh", user+"@"+server)
 
 	// Set the standard input, output, and error streams to the current process's streams
@@ -30,6 +42,6 @@ func Connect(server, user string) {
 
 	ssh_err := sshCommand.Run()
 	if ssh_err != nil {
-		fmt.Println("Failed to run SSH command:", err)
+		fmt.Println("Failed to run SSH command:", ssh_err)
 	}
 }
