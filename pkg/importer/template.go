ed7401ae9f300b3fabb471dd168d23fe870e717a
AshutoshPatole <apatole479@gmail.com>
2023-09-19 21:55:58 +0530

    add: template works

--- pkg/importer/template.go
+++ pkg/importer/template.go
@@ -5,8 +5,11 @@ package importer
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/TwiN/go-color"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
 
 // templateCmd represents the template command
@@ -16,6 +19,7 @@ var templateCmd = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("template called")
+		saveTemplate()
 	},
 }
 
@@ -31,4 +35,52 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// templateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func saveTemplate() {
+	content := `
+- name: groupname
+  user: username
+  env:
+    - name: dev|uat|sit|ppd|prd
+      servers:
+        - hostname: gvx0lcmdme01d.aholdusa.com
+          alias: dev engine
+        - hostname: gvx0lcmdms01d.aholdusa.com
+          alias: dev service
+        - hostname: gvx0lcmdme01p.aholdusa.com
+          alias: prod engine
+
+- name: groupname2
+  user: anotheruser
+  env:
+    - name: dev|uat|sit|ppd|prd
+      servers:
+        - hostname: gvx0lcmdme01d.aholdusa.com
+          alias: dev engine
+        - hostname: gvx0lcmdms01d.aholdusa.com
+          alias: dev service
+        - hostname: gvx0lcmdme01p.aholdusa.com
+          alias: prod engine
+`
+	// fmt.Println(content)
+	data := []Group{}
+	err := yaml.Unmarshal([]byte(content), &data)
+	if err != nil {
+		fmt.Println("something wrong in unmarshaling", err.Error())
+		return
+	}
+	d, err := yaml.Marshal(&data)
+	if err != nil {
+		fmt.Println("something wrong in marshaling")
+		return
+	}
+	fmt.Println(string(d))
+	homeDir, _ := os.UserHomeDir()
+	wErr := os.WriteFile(homeDir+"/ssh-import-template.yaml", d, 0666)
+	if wErr != nil {
+		fmt.Println("error while saving file", wErr.Error())
+		return
+	}
+	fmt.Println(color.InGreen("File saved at " + homeDir + "/ssh-import-template.yaml"))
 }
